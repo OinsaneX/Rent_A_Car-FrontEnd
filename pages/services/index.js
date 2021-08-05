80e697686f038d7cff2fdfad4f2d4269f298ebea
OinsaneX <alejandroalfonsovargas@gmail.com>
2021-08-04 18:09:03 -0700

    Updating rent model

--- pages/services/index.js
+++ pages/services/index.js
@@ -1,86 +1,71 @@
-import Support from '../../svgs/img/Support'
-import Work from '../../svgs/img/Work'
-import About from '../../svgs/img/About'
-import NavBar from '../../components/NavBar';
+import Support from "../../svgs/img/Support";
+import Work from "../../svgs/img/Work";
+import About from "../../svgs/img/About";
+import NavBar from "../../components/NavBar";
+import Link from "next/link";
 
+export default function Services() {
+	return (
+		<>
+			<NavBar />
+			<main>
+				<section>
+					<div className="card">
+						<h3>Soporte</h3>
+						<header>
+							<Support />
+						</header>
 
-export default function() {
-    return (
-        <>
-        <NavBar/>
-            <main>
+						<h4>Contacte con algun administrador en caso de dudas</h4>
+						<Link href="/support"><a>Accesar</a></Link>
+					</div>
+					<div className="card">
+						<h3>Empleo</h3>
 
-                <section>
-                <div className="card">
-                    <h3>Soporte</h3>
-                    <header>
-                    <Support/>
-                    </header>
+						<header>
+							<Work />
+						</header>
 
-                    <h4>
-                    Contacte con algun administrador en caso de dudas
-                    </h4>
-                    <button>
-                        Accesar
-                    </button>
-                </div>
-                <div className="card">
-                <h3>Empleo</h3>
+						<h4>Envie formulario para trabajar con nosotros como chofer</h4>
+						<Link href="/workForm"><a>Enviar</a></Link>
+					</div>
+					<div className="card">
+						<h3>Sobre</h3>
 
-                    <header>
-                    <Work/>
-                    </header>
+						<header>
+							<About />
+						</header>
 
-                    <h4>
-                    Envie formulario para trabajar con nosotros como chofer
-                    </h4>
-                    <button>
-                        Enviar
-                    </button>
-                </div>
-                <div className="card">
-                <h3>Sobre</h3>
+						<h4>Descubra el equipo de la empresa</h4>
+						<Link href="/discover"><a>Descubrir</a></Link>
+					</div>
+				</section>
+			</main>
 
-                    <header>
-                    <About/>
-                    </header>
+			<style jsx>{`
+				section {
+					display: flex;
+					justify-content: center;
+					flex-wrap: wrap;
+					text-align: center;
+				}
+				.card {
+					display: grid;
+					place-items: center;
+					place-content: center;
+					box-shadow: 0px 0px 5px rgba(0, 0, 0, 1);
+					margin: 20px;
+					width: 400px;
+				}
 
-                    <h4>
-                    Descubra el equipo de la empresa
-                    </h4>
-                    <button>
-                        Descubrir
-                    </button>
-                </div>
-                </section>
-            </main>
-
-            <style jsx>{`
-            section{
-                display:flex;
-                justify-content:center;
-                flex-wrap:wrap;
-                text-align:center;
-                
-            }
-            .card{
-                display:grid;
-                place-items:center;
-                place-content:center;
-                box-shadow: 0px 0px 5px rgba(0, 0, 0,1);
-                margin:20px;
-                width:400px
-            }
-
-            button{
-                margin:10px;
-                padding:10px 20px;
-                border: none;
-                background-color: #000;
-                color: #fff;
-            }
-
-            `}</style>
-        </>
-    );
+				a {
+					margin: 10px;
+					padding: 10px 20px;
+					border: none;
+					background-color: #000;
+					color: #fff;
+				}
+			`}</style>
+		</>
+	);
 }
---
+++ pages/workForm/index.js
@@ -0,0 +1,164 @@
+import axios from "axios";
+import { useState } from "react";
+
+export default function WorkForm() {
+
+        const [driverForm, setdriverForm] = useState({licenseUrl:""})
+        const [loading, setloading] = useState(undefined)
+
+    function onAddImage(e) {
+        e.preventDefault();
+        var file = e.target.files[0];
+        var formData = new FormData();
+        formData.append("file", file);
+        formData.append(
+          "upload_preset",
+          'u45m9vyq'
+        );
+        setloading(1);
+        axios
+          .post(
+            `https://api.cloudinary.com/v1_1/dxyv7aypq/image/upload`,
+            formData
+          )
+          .then((response) => {
+            setloading(null);
+    
+            setdriverForm({ ...driverForm, licenseUrl: response.data.secure_url });
+          })
+          .catch((error) => {console.log(error)})
+          ;
+          
+      }
+
+	return (
+		<>
+			<main>
+				<header>
+					<h3>
+						Rellene el formulario con sus datos para solicitar una plaza como
+						conductor
+					</h3>
+					<h4>
+						La respuesta puede demorar hasta una semana ...Debe esperar a un
+						administrador ver su perfil y sus datos y será comunicado via email
+						o teléfono
+					</h4>
+				</header>
+				<section>
+					<form>
+                    <div className="col">
+                        <h5>Nombre :</h5>
+                    <input type="text" />
+                    </div>
+                    <div className="col">
+                        <h5>Email :</h5>
+                    <input type="text" />
+                    </div>
+                    <div className="col">
+                        <h5>CI :</h5>
+                    <input type="text" />
+                    </div>
+                    <div className="col">
+                        <h5>Teléfono :</h5>
+                    <input type="text" />
+                    </div>
+                    <div className="col">
+                        <h5>Nacionalidad :</h5>
+                    <input type="text" />
+                    </div>
+                    <div className="col">
+                        <h5>Dirección :</h5>
+                    <input type="text" />
+                    </div>
+                    <div className="col">
+                        <h5>Años de Experiencia :</h5>
+                    <input type="number" />
+                    </div>
+                    <div className="col">
+                        <h5>Licencia :</h5>
+                    <input type="text" />
+                    </div>
+                    <div className="col">
+                        <h5>Fecha de Validación :</h5>
+                    <input type="number" />
+                    </div>
+                    <div className="curriculum">
+                        <h5>Currículo :</h5>
+                    <textarea  />
+                    </div>
+                    <div className="file">
+                    <label  htmlFor="upload">Escoger imagen</label>
+                        <input id="upload" type="file" accept="image/*" onChange={(e)=>onAddImage(e)} placeholder="imagen"/>
+
+                        <img src={driverForm.licenseUrl} alt="" />
+                    </div>
+                    </form>
+				</section>
+			</main>
+
+            <style jsx>{`
+
+            .file{
+                margin:20px;
+                display:flex;
+                flex-direction:column;
+            }
+
+            label {
+                text-align: center;
+                padding: 10px 20px;
+                background-color: #000;
+                color: #fff;
+            }
+
+            header{
+                background-color: #000;
+                color: #fff;
+                display:flex;
+                flex-direction:column;
+                text-align:center;
+            }
+
+            form {
+                display:flex;
+                align-items:center;
+                place-content:center;
+                flex-wrap:wrap;
+            }
+            input[type="file"]{
+                display:none;
+            }
+            .curriculum{
+                width:90%;
+                display:flex;
+                place-items:center;
+
+                flex-direction:column;
+                justify-content:space-between;
+                margin:10px 20px;
+            }
+
+            .col{
+                width:300px;
+                display:flex;
+                justify-content:space-between;
+                margin:10px 20px;
+            }
+            input{
+                height:40px;
+            }
+
+            textarea {
+                width: 100%;
+                height: 300px
+            }
+
+            img{
+                max-width:300px;
+                max-height: 250px;
+            }
+            `}</style>
+		</>
+	);
+}
